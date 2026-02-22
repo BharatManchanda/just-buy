885e9760920ce65edc3dbd678815aac8a7ca80f7
Bharat Manchanda <bharatmanchanda13@gmail.com>
2026-02-22 18:04:40 +0530

    update

--- Server/DB/connect.js
+++ Server/DB/connect.js
@@ -1,18 +1,45 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 require("dotenv").config();
 
-const connectDB = async () => {
-  try {
-    console.log("Connecting to MongoDB...");
-    // await mongoose.connect(process.env.CONNECTION_STRING);
-    await mongoose.connect(process.env.CONNECTION_STRING, {
+// const connectDB = async () => {
+//   try {
+//     console.log("Connecting to MongoDB...");
+//     // await mongoose.connect(process.env.CONNECTION_STRING);
+//     await mongoose.connect(process.env.CONNECTION_STRING, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("MongoDB connected");
+//   } catch (err) {
+//     console.error("MongoDB connection failed:", err.message);
+//   }
+// };
+
+// module.exports = connectDB;
+
+// DB/connect.js
+const mongoose = require("mongoose");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (err) {
-    console.error("MongoDB connection failed:", err.message);
+    }).then((mongoose) => mongoose);
   }
-};
 
-module.exports = connectDB;
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+module.exports = connect;
