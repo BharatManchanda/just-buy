1d3ceaedd29ecb1a2270a442866f2369037a2e76
Bharat Manchanda <bharatmanchanda13@gmail.com>
2026-02-22 17:52:55 +0530

    update

--- Server/DB/connect.js
+++ Server/DB/connect.js
@@ -4,7 +4,11 @@ require("dotenv").config();
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
-    await mongoose.connect(process.env.CONNECTION_STRING);
+    // await mongoose.connect(process.env.CONNECTION_STRING);
+    await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
